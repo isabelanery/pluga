29e18bd22b7056159a8b648e1565be9a8fd11d75
Isabela Nery <isabelanery.r@gmail.com>
2022-10-03 17:40:06 -0300

    feat: start testing, mock api

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,27 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import axios from 'axios';
 import App from '../App';
+import mockApi from './mocks/api';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('The page is rendered correctly', () => {
+  beforeEach(() => {
+    jest.mock("axios");
+    axios.get.mockImplementation(() => Promise.resolve({ data: mockApi }));
+  });
+
+  afterEach(() => jest.clearAllMocks());
+
+  it('has a button to switch pages', () => {
+    render(<App />);
+    const toolCards = screen.getByRole("button");
+    expect(toolCards).toBeInTheDocument();
+  });
+
+  it('list the tools available in the API', () => {
+    render(<App />);
+    const toolCards = screen.getByRole("img");
+    expect(toolCards).toBeInTheDocument();
+  });
 });
