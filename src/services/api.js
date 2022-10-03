b31ea198f4309cf219c62259b8a292d3a9d8b46f
Isabela Nery <isabelanery.r@gmail.com>
2022-10-03 20:25:20 -0300

    refactor: api to make it more dynamic and readable

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
-const baseURL = 'https://pluga.co/ferramentas_search.json';
+const BASE_URL = 'https://pluga.co';
+const GET_TOOLS_PATH = '/ferramentas_search.json';
 
-export default () => axios.get(baseURL);
+const fetchData = async (path) => {
+  const response = await axios.get(`${BASE_URL}${path}`);
+  return response || null;
+};
+
+const getTools = async () => {
+  const response = await fetchData(GET_TOOLS_PATH);
+  return response || [];
+};
+
+const api = {
+  fetchData,
+  getTools,
+};
+
+export default api;
