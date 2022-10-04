a6a90917f237687b364084b3d8411a3312667333
Isabela Nery <isabelanery.r@gmail.com>
2022-10-03 23:34:59 -0300

    refactor: conditional rendering

--- src/components/LastOpenedToolsRow/index.jsx
+++ src/components/LastOpenedToolsRow/index.jsx
@@ -2,27 +2,36 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import ToolCard from '../ToolCard';
 import './LastOpenedToolsRow.css';
+import storage from '../../services/storage';
 
 export default function LastOpenedToolsRow(props) {
   const { isOpen } = props;
   const [list, setList] = useState([]);
 
   useEffect(() => {
-    const lastOpened = JSON.parse(localStorage.getItem('lastOpened')) || [];
-    if (lastOpened.length > 3) lastOpened.shift();
-    setList(lastOpened);
+    const lastOpenedTools = storage.getLastOpenedTools();
+    setList(lastOpenedTools);
   }, [isOpen]);
 
   return (
     <div className="last-opened-container">
-      Últimas ferramentas visualizadas
-      <div className="last-opened">
-        { // refactor loading
-          list.length > 0 && list.map((app, index) => (
-            <ToolCard small data={app} key={`last-opened-${index + 1}`} />
-          ))
-        }
-      </div>
+      {
+      list.length > 0
+        && (
+          <>
+            <p>
+              Últimas ferramentas visualizadas
+            </p>
+            <div className="last-opened">
+              {
+              list.map((app, index) => (
+                <ToolCard small data={app} key={`last-opened-${index + 1}`} />
+              ))
+              }
+            </div>
+          </>
+        )
+      }
     </div>
   );
 }
