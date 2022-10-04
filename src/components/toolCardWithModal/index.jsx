be00812d75cb9d44c7d0131e972d162aec4aa7a8
Isabela Nery <isabelanery.r@gmail.com>
2022-10-03 23:32:31 -0300

    refactor: separete logic of updating last opened list from the component

--- src/components/toolCardWithModal/index.jsx
+++ src/components/toolCardWithModal/index.jsx
@@ -2,37 +2,19 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../Modal';
 import ToolCard from '../ToolCard';
+import storage from '../../services/storage';
 
-export default function CardWithModal(props) {
+export default function ToolCardWithModal(props) {
   const { data } = props;
   const [isOpen, setIsOpen] = useState(false);
 
-  const closeModal = () => setIsOpen(false);
-
-  const listLastOpenedTools = () => {
-    const lastOpened = JSON.parse(localStorage.getItem('lastOpened'));
-
-    if (!lastOpened) {
-      localStorage.setItem('lastOpened', JSON.stringify([data]));
-      setIsOpen(true);
-      return null;
-    }
-
-    const duplicate = lastOpened.find((item) => item.name === data.name);
-
-    if (lastOpened.indexOf(duplicate) === 0) lastOpened.splice(lastOpened.indexOf(duplicate), 1);
-
-    lastOpened.unshift(data);
-
-    if (lastOpened.length > 4) lastOpened.pop();
-
-    localStorage.setItem('lastOpened', JSON.stringify(lastOpened));
-    return null;
+  const openModal = () => {
+    setIsOpen(true);
   };
 
-  const openModal = () => {
-    listLastOpenedTools();
-    setIsOpen(true);
+  const closeModal = () => {
+    storage.updateLastOpenedTools(data);
+    setIsOpen(false);
   };
 
   return (
@@ -44,6 +26,6 @@ export default function CardWithModal(props) {
   );
 }
 
-CardWithModal.propTypes = {
+ToolCardWithModal.propTypes = {
   data: PropTypes.objectOf(PropTypes.string),
 }.isRequired;
