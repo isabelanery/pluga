9162a89f36e88d69ef661a7b5369ce8c3d42edc3
Isabela Nery <isabelanery.r@gmail.com>
2022-10-03 23:33:33 -0300

    feat: localStorage interaction

---
+++ src/services/storage.js
@@ -0,0 +1,41 @@
+const LAST_OPENED_TOOLS = 'lastOpenedTools';
+
+const getFromLocalStorage = (key) => {
+  const response = localStorage.getItem(key);
+  return JSON.parse(response) || null;
+};
+
+const getLastOpenedTools = () => {
+  const response = getFromLocalStorage(LAST_OPENED_TOOLS);
+  return response || [];
+};
+
+const setLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+
+const addToLastOpenedTools = (data) => setLocalStorage(LAST_OPENED_TOOLS, data);
+
+const updateLastOpenedTools = (data) => {
+  const lastOpenedList = getLastOpenedTools();
+
+  if (lastOpenedList.length <= 0) addToLastOpenedTools([data]);
+
+  const duplicated = lastOpenedList.find((item) => item.name === data.name);
+
+  if (duplicated) lastOpenedList.splice(lastOpenedList.indexOf(duplicated), 1);
+
+  lastOpenedList.unshift(data);
+
+  if (lastOpenedList.length > 3) lastOpenedList.pop();
+
+  return addToLastOpenedTools(lastOpenedList);
+};
+
+const storage = {
+  getFromLocalStorage,
+  setLocalStorage,
+  getLastOpenedTools,
+  addToLastOpenedTools,
+  updateLastOpenedTools,
+};
+
+export default storage;
